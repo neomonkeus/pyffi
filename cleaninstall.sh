8105319454df57113d5143a6fabe06041c6b644e
neomonkeus <neomonkeus@gmail.com>
2020-05-07 21:30:07 +0100

    Remove files which are nolonger needed.

--- cleaninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-git clean -f -d -x
-python3 setup.py -q build
-python3 setup.py -q sdist --format=zip
-sudo pip-python3 uninstall PyFFI -y
-sudo pip-python3 install dist/PyFFI-*.zip
-rm -rf build
-
